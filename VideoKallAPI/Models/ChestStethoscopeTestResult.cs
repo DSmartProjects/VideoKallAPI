2bac8e4603cac907f206a7564c0208de07d99307
Rama Boddeda <rama.boddeda@dsmartsys.in>
2020-12-29 10:05:56 +0530

    Added Recording_Path in ChestStethoscopeTestResult model

--- VideoKallAPI/Models/ChestStethoscopeTestResult.cs
+++ VideoKallAPI/Models/ChestStethoscopeTestResult.cs
@@ -12,6 +12,8 @@ namespace VideoKallAPI.Models
         public int? ChairId { get; set; }
         public Guid? SessionId { get; set; }
         public byte[] Recording { get; set; }
+        public string Recording_Path { get; set; }
+        
         public int? CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
 
